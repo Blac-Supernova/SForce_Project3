6844d48c49e1b417ca73aff7296ce9a4dc2ffd82
Arashdeep Singh <datonearash@gmail.com>
2024-05-23 12:57:41 -0700

    added more info to record view form

--- force-app/main/default/lwc/propertyViewPage/propertyViewPage.js
+++ force-app/main/default/lwc/propertyViewPage/propertyViewPage.js
@@ -5,19 +5,19 @@ import address from '@salesforce/schema/Property__c.Location__c'
 import  numberOfBaths  from '@salesforce/schema/Property__c.Number_of_Bathrooms__c';
 import  numberOfBedrooms  from '@salesforce/schema/Property__c.Number_of_Bedrooms__c';
 import image from '@salesforce/schema/Property__c.Image_URL__c';
+import description from '@salesforce/schema/Property__c.Description__c';
+import petsAllowed from '@salesforce/schema/Property__c.Are_Pets_Allowed__c';
+import square from '@salesforce/schema/Property__c.sqft__c';
 
 export default class PropertyViewPage extends NavigationMixin(LightningElement) {
     @track
     currentPageReference;
-
-    
 
     @wire(CurrentPageReference)
     setCurrentPageReference(currentPageReference) {
         this.currentPageReference = currentPageReference;
     }
 
-    
     propertyId; 
 
     connectedCallback() {
@@ -25,12 +25,12 @@ export default class PropertyViewPage extends NavigationMixin(LightningElement) 
     }
 
     objectApiName = residenceReference;
-
+    
     numberOfBaths = numberOfBaths;
-
     numberOfBedrooms = numberOfBedrooms;
-
     imageURL = image;
-
     address=address;
+    square = square;
+    description = description;
+    petsAllowed = petsAllowed;
 }
