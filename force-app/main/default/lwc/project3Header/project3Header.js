98cd5cd85254d07e19c146d1b93eaac18a9bc1c3
Thesm1thy <jsmit30@unh.newhaven.edu>
2024-05-24 18:39:26 -0400

    Added Header Files
    Header files

---
+++ force-app/main/default/lwc/project3Header/project3Header.js
@@ -0,0 +1,63 @@
+import { LightningElement, wire, track } from 'lwc';
+import GeniePropLogo from '@salesforce/resourceUrl/genielogo';
+// import isUserLoggedin from '@salesforce/apex/sessionHelper.isUserLoggedIn';
+import isGuest from '@salesforce/user/isGuest'
+import basePath from "@salesforce/community/basePath";
+
+
+
+export default class Project3Header extends LightningElement {
+    companyLogo = GeniePropLogo;
+    isNotGuest = !isGuest;
+
+
+    get logoutLink() {
+        const sitePrefix = basePath.replace("/", "");
+        return '/${sitePrefix}vforcesite/secur/logout.jsp';
+    }
+
+   //hard code
+    //isLoggedin = false;
+
+    //Attempt #2
+    // @track isLoggedIn = false;
+
+    // connectedCallback() {
+    //     this.checkUserLoginStatus();
+    // }
+
+
+    // async checkUserLoginStatus() {
+    //     try {
+    //         const sessionId = await isUserLoggedin();
+    //         console.log('sessionId', sessionId);
+    //         this.isLoggedIn = sessionId ? true : false;
+    //         console.log('isLoggedIn', this.isLoggedIn);
+    //     }
+    //     catch (error) {
+    //         console.error('Error checking login status', error);
+    //         this.isLoggedIn = false;
+    //     }
+    // }
+
+
+    // handleClick() {
+    //     if (this.isLoggedIn) {
+    //         this.logoutUser();
+    //     } else {
+    //         this.redirectToLogin();
+    //     }
+    // }
+
+
+    // logoutUser() {
+    //     window.location.href = '/secur/logout.jsp';
+    // }
+
+    // redirectToLogin() {
+    //     window.location.href = '/login';
+
+    // }
+
+    
+}
