7822e4d06f79302b9877e53ddbd7579a0fa142f4
Gamma631 <55938393+Gamma631@users.noreply.github.com>
2024-05-31 11:58:04 -0400

    Pushed the full implementation of my use case
    Pushed the full implementation of my use case, the monthly statements component.

--- force-app/main/default/lwc/addFamily/addFamily.js
+++ force-app/main/default/lwc/addFamily/addFamily.js
@@ -1,58 +1,53 @@
-
-import { LightningElement, track } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { createRecord } from 'lightning/uiRecordApi';
 import CONTACT_OBJECT from '@salesforce/schema/Contact';
-import id from '@salesforce/user/Id';
-import getFamilyMembers from '@salesfroce/apex/ContactHelper.getFamilyMembers';
-export default class AddFamily extends LightningElement {
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
+import TITLE_FIELD from '@salesforce/schema/Contact.Title';
+import PROPERTY_FIELD from '@salesforce/schema/Contact.Property__c';
+import getFamilyMembers from '@salesforce/apex/ContactHelper.getFamilyMembers';
+import Id from '@salesforce/user/Id';
+
+export default class LdsCreateRecord extends LightningElement {
     
-    @track firstName = '';
-    @track lastName = '';
-    @track email = '';
-    @track phone = '';
-    @track title = '';
-    userId = id;
+    firstName = '';
+    lastName = '';
+    phone = '';
+    title = '';
+    userId = Id;
+
 
     @wire(getFamilyMembers, {userId : "$userId"})
     familyMembers;
 
-
-
-    handleFirstNameChange(event) {
-        this.firstName = event.target.value;
-    }
-
-    handleLastNameChange(event) {
-        this.lastName = event.target.value;
-    }
-
-    handleEmailChange(event) {
-        this.email = event.target.value;
-    }
-
-    handlePhoneChange(event) {
-        this.accRecord.Phone = event.target.value;
-    }
-
-    handleTitleChange(event) {
-        this.title = event.target.value;
-    }
-
-    handleSaveClick() {
-        const fields = {
-            FirstName: this.firstName,
-            LastName: this.lastName,
-            Email: this.email,
-            Phone: this.phone,
-            Title: this.title
-        };
+   
+    /*async createContact() {
+        const fields = {};
+        fields[FIRSTNAME_FIELD.fieldApiName] = this.template.refs.firstName.value;
+        fields[LASTNAME_FIELD.fieldApiName] = this.template.refs.lastName.value;
+        fields[PHONE_FIELD.fieldApiName] = this.template.refs.phone.value;
+        fields[TITLE_FIELD.fieldApiName] = this.template.refs.title.value;
+        //fields[PROPERTY_FIELD.fieldApiName] = familyMembers.Data[0].Property__c;
         const recordInput = { apiName: CONTACT_OBJECT.objectApiName, fields };
-        createRecord(recordInput)
-            .then(result => {
-                console.log('Contact created successfully:', result.id);
-            })
-        .catch(error => {
-            this.error = error.message;
-        });
-    }
+        try {
+            const contact = await createRecord(recordInput);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Contact created.',
+                    variant: 'success'
+                })
+            );
+        } catch (error) {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error creating record',
+                    message: 'Error in creating contact.',
+                    variant: 'error'
+                })
+            );
+        }
+    } */
 }
--- force-app/main/default/lwc/monthlyStatements/monthlyStatements.js
+++ force-app/main/default/lwc/monthlyStatements/monthlyStatements.js
@@ -3,24 +3,18 @@ import getMonthlyStatementsByUserId from '@salesforce/apex/MonthlyStatementsCont
 import isGuest from '@salesforce/user/isGuest';
 import Id from '@salesforce/user/Id';
 
-const columns = [
-    {label: 'Id', fieldName: 'Name'},
-    {label: 'Monthly Amount', fieldName: 'Monthly_Amount__c', type: 'currency', cellAttributes: {alignment: 'left'}}
-];
-
 export default class MonthlyStatements extends LightningElement {
     @api recordId;
+    loggedIn = false;
     monthlyStatements;
     error;
-	loggedIn = !isGuest;
     usersId = Id;
 
-    columns = columns;
-
-    @wire(getMonthlyStatementsByUserId, {userId: '$usersId'})
+    @wire(getMonthlyStatementsByUserId, {userId: Id})
     wiredStatements({error, data}) {
         if (data) {
             this.monthlyStatements = data;
+            this.loggedIn = (!isGuest && this.monthlyStatements.length > 0);
             this.error = undefined;
         } else if (error) {
             this.error = error;
